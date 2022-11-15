7ceb6d9a410279291f0d6832c82d90ee7b3640b1
Tanya <tanyasharma2614@gmail.com>
2022-11-15 18:36:27 -0500

    Filter seq scan implemented

--- src/filter_seq_scan_executor.cpp
+++ src/filter_seq_scan_executor.cpp
@@ -4,6 +4,15 @@ FilterSeqScanExecutor::FilterSeqScanExecutor(Table *table,
                                              FilterPredicate *pred)
     : table_(table), pred_(pred){};
 
-void FilterSeqScanExecutor::Init() {}
+void FilterSeqScanExecutor::Init() {iter_=table_->Begin();}
 
-bool FilterSeqScanExecutor::Next(Tuple *tuple) { return false; }
+bool FilterSeqScanExecutor::Next(Tuple *tuple) { 
+    while(iter_!=table_->End()){
+        const Tuple &curr_tuple=*iter_;
+        *tuple=Tuple(curr_tuple);
+        ++iter_;
+        if(pred_->evaluate(*tuple))
+        return true;
+    }
+    return false; 
+}
