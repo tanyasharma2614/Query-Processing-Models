f41308ab2225edc336ea7c251bf9e6757fcb627b
Tanya <tanyasharma2614@gmail.com>
2022-11-23 18:20:09 -0500

    Iteration working

--- src/nested_loop_join_executor.cpp
+++ src/nested_loop_join_executor.cpp
@@ -1,4 +1,10 @@
 #include "../include/nested_loop_join_executor.h"
+#include "../include/seq_scan_executor.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
 
 NestedLoopJoinExecutor::NestedLoopJoinExecutor(
     AbstractExecutor *left_child_executor,
@@ -7,6 +13,20 @@ NestedLoopJoinExecutor::NestedLoopJoinExecutor(
       right_(right_child_executor),
       join_key_(join_key){};
 
-void NestedLoopJoinExecutor::Init() {}
+void NestedLoopJoinExecutor::Init() {
+  left_->Init();
+  right_->Init();
+}
 
-bool NestedLoopJoinExecutor::Next(Tuple *tuple) { return false; }
+bool NestedLoopJoinExecutor::Next(Tuple *tuple) { 
+  Tuple t1;
+  Tuple t2;
+  while(left_->Next(&t1)){
+    while(right_->Next(&t2)){
+      *tuple=Tuple(t1);
+      return true;
+    }
+  }
+  return false;
+  
+}
